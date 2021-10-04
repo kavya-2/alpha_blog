b0c3edcf4e28ecdeda47990f5e23a10d9ddb12b8
Kavya S <kavya.s2@happiestminds.com>
2021-10-04 14:43:16 +0530

    add sign up

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
+    belongs_to :user
     validates :title, presence: true, length: { minimum: 3, maximum: 100 }
     validates :description, presence: true, length: { minimum: 4, maximum: 300 }
-    belongs_to :user
 end
